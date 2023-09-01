b266c8faafa5d740297c0f4717aed6fb30098ccc
shbrown@webstaurantstore.com <shbrown@webstauranstore.com>
2023-08-31 20:47:41 -0400

    drone movement added

--- drone/Drone.cs
+++ drone/Drone.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public class Drone : KinematicBody, IShootable
 {
@@ -7,7 +6,20 @@ public class Drone : KinematicBody, IShootable
     private Spatial _gunBarrel;
     private float _fireCooldown = 2f;
     private Spatial _target;
-    private bool _falling = true;
+    private float _speed = 5f;
+    private int _health = 3;
+    private Area _liftArea;
+    private enum DroneState
+    {
+        Idle,
+        Falling,
+        Hit,
+        Firing,
+        MovingForward,
+        MovingBackward,
+        Dead,
+    }
+    private DroneState _state;
 
     public override void _Ready()
     {
@@ -15,8 +27,13 @@ public class Drone : KinematicBody, IShootable
         _animationPlayer.Connect("animation_finished", this, nameof(AnimationEnded));
 
         _gunBarrel = GetNode<Spatial>("GunBarrel");
+        _liftArea = GetNode<Area>("LiftArea");
 
         _target = GetTree().Root.FindNode("Player", true, false) as Spatial;
+
+        _state = DroneState.Falling;
+        _animationPlayer.Play("Falling");
+
     }
 
     public override void _Process(float delta)
@@ -24,33 +41,139 @@ public class Drone : KinematicBody, IShootable
         if (_target != null)
             LookAt(_target.GlobalTransform.origin, Vector3.Up);
 
-        if (_fireCooldown > 0 && CanFireBullet())
-            _fireCooldown -= delta;
-
-        if (_fireCooldown <= 0)
+        switch (_state)
         {
-            _fireCooldown = 1.5f;
-            FireBullet();
+            case DroneState.Idle:
+                if (_target != null)
+                {
+                    if (GlobalTranslation.DistanceTo(_target.GlobalTranslation) > 12)
+                    {
+                        _state = DroneState.MovingForward;
+                        _animationPlayer.Play("Forward");
+                    }
+                    else if (GlobalTranslation.DistanceTo(_target.GlobalTranslation) < 5)
+                    {
+                        _state = DroneState.MovingBackward;
+                        _animationPlayer.Play("Backward");
+                    }
+                }
+                break;
+            case DroneState.Falling:
+                if (GlobalTranslation.y <= 2)
+                {
+                    _state = DroneState.Idle;
+                    _animationPlayer.Play("FinishFalling");
+                }
+                break;
+            case DroneState.Hit:
+                break;
+            case DroneState.Firing:
+                break;
+            case DroneState.MovingForward:
+                if (_target != null)
+                {
+                    if (GlobalTranslation.DistanceTo(_target.GlobalTranslation) < 6)
+                    {
+                        _state = DroneState.Idle;
+                        _animationPlayer.Play("Idle");
+                    }
+                }
+                else
+                {
+                    _state = DroneState.Idle;
+                    _animationPlayer.Play("Idle");
+                }
+                break;
+            case DroneState.MovingBackward:
+                if (_target != null)
+                {
+                    if (GlobalTranslation.DistanceTo(_target.GlobalTranslation) > 10)
+                    {
+                        _state = DroneState.Idle;
+                        _animationPlayer.Play("Idle");
+                    }
+                }
+                else
+                {
+                    _state = DroneState.Idle;
+                    _animationPlayer.Play("Idle");
+                }
+                break;
+            default:
+                break;
         }
+    }
+    public override void _PhysicsProcess(float delta)
+    {
+        switch (_state)
+        {
+            case DroneState.Idle:
+                break;
+            case DroneState.Falling:
+                MoveAndSlide(Vector3.Down * 15, Vector3.Up);
+                break;
+            case DroneState.Hit:
+                break;
+            case DroneState.Firing:
+                break;
+            case DroneState.MovingForward:
+                if (_target != null)
+                {
+                    var forward = GlobalTranslation.DirectionTo(new Vector3(_target.GlobalTranslation.x, GlobalTranslation.y, _target.GlobalTranslation.z));
+                    MoveAndSlide(forward * _speed, Vector3.Up);
 
-        if (_falling && GlobalTranslation.y <= 2)
-        {
-            _falling = false;
-            _animationPlayer.Play("FinishFalling");
-        }
-        else if (GlobalTranslation.y > 2)
-        {
-            _animationPlayer.Play("Falling");
-            _falling = true;
+                    if (_liftArea.GetOverlappingBodies().Count > 1 && GlobalTranslation.y < 3)
+                    {
+                        MoveAndSlide(Vector3.Up * _speed * .5f, Vector3.Up);
+                    }
+                    else if (_liftArea.GetOverlappingBodies().Count == 1 && GlobalTranslation.y > 2)
+                    {
+                        MoveAndSlide(Vector3.Down * _speed * .2f, Vector3.Up);
+                    }
+                }
+                break;
+            case DroneState.MovingBackward:
+                if (_target != null)
+                {
+                    var back = new Vector3(_target.GlobalTranslation.x, GlobalTranslation.y, _target.GlobalTranslation.z).DirectionTo(GlobalTranslation);
+                    MoveAndSlide(back * _speed, Vector3.Up);
+
+                    if (_liftArea.GetOverlappingBodies().Count > 1 && GlobalTranslation.y < 3)
+                    {
+                        MoveAndSlide(Vector3.Up * _speed * .5f, Vector3.Up);
+                    }
+                    else if (_liftArea.GetOverlappingBodies().Count == 1 && GlobalTranslation.y > 2)
+                    {
+                        MoveAndSlide(Vector3.Down * _speed * .2f, Vector3.Up);
+                    }
+                }
+                break;
+            default:
+                break;
         }
     }
 
-    public override void _PhysicsProcess(float delta)
+
+    public void AnimationEnded(string animationName)
     {
-        if (_falling)
+        switch (_state)
         {
-            MoveAndSlide(Vector3.Down * 15, Vector3.Up);
-            _falling = true;
+            case DroneState.Falling:
+                _animationPlayer.Play("Falling");
+                break;
+            case DroneState.MovingForward:
+                _animationPlayer.Play("Forward");
+                break;
+            case DroneState.MovingBackward:
+                _animationPlayer.Play("Backward");
+                break;
+            case DroneState.Hit:
+                _state = DroneState.Idle;
+                _animationPlayer.Play("Idle");
+                break;
+            default:
+                _animationPlayer.Play("Idle");
+                break;
         }
     }
 
@@ -73,18 +196,13 @@ public class Drone : KinematicBody, IShootable
     {
         _animationPlayer.Stop();
         _animationPlayer.Play("Hit");
-    }
-
-    public void AnimationEnded(string animationName)
-    {
-        if (_falling)
+        _state = DroneState.Hit;
+        _health--;
+        if (_health <= 0)
         {
-            _animationPlayer.Play("Falling");
-        }
-        else
-        {
-            _animationPlayer.Play("Idle");
+            _state = DroneState.Dead;
+            BulletHole.RemoveBulletHoles(this);
+            CallDeferred("queue_free");
         }
     }
-
 }
--- drone/DroneSpawner.cs
+++ drone/DroneSpawner.cs
@@ -6,9 +6,21 @@ public class DroneSpawner : Spatial
     [Export]
     PackedScene DroneScene;
 
-    public override void _Ready()
+    private float _timer;
+    private float _spawnInterval = 5f;
+    private int _spawnCount = 5;
+
+    public override void _Process(float delta)
     {
-        SpawnDrone();
+        if (_timer < _spawnInterval)
+            _timer += delta;
+
+        if (_spawnCount > 0 && _timer > _spawnInterval)
+        {
+            _timer = 0;
+            _spawnCount--;
+            SpawnDrone();
+        }
     }
 
     public void SpawnDrone()
@@ -17,5 +29,10 @@ public class DroneSpawner : Spatial
         var mainScene = GetTree().Root.FindNode("Main", true, false);
         mainScene.CallDeferred("add_child", drone);
         drone.CallDeferred("set_translation", GlobalTransform.origin + Vector3.Up * 20);
+    }
+
+    public void SetSpawnCount(int count)
+    {
+        _spawnCount = count;
     }
 }
