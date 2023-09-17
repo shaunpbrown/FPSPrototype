5be1204042461144ed091c29920bf38c7e1a0fe1
shbrown@webstaurantstore.com <shbrown@webstauranstore.com>
2023-09-16 23:58:16 -0400

    rounds added

--- player/scripts/RoundInformation.cs
+++ player/scripts/RoundInformation.cs
@@ -36,6 +36,41 @@ public class RoundInformation
                 _droneSpawner.MaxAliveCount = 2;
                 SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
                 break;
+            case 4:
+                _droneSpawner.MaxSpawnCount = 6;
+                _droneSpawner.MaxAliveCount = 4;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
+            case 5:
+                _droneSpawner.MaxSpawnCount = 8;
+                _droneSpawner.MaxAliveCount = 6;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
+            case 6:
+                _droneSpawner.MaxSpawnCount = 10;
+                _droneSpawner.MaxAliveCount = 8;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
+            case 7:
+                _droneSpawner.MaxSpawnCount = 12;
+                _droneSpawner.MaxAliveCount = 10;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
+            case 8:
+                _droneSpawner.MaxSpawnCount = 14;
+                _droneSpawner.MaxAliveCount = 12;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
+            case 9:
+                _droneSpawner.MaxSpawnCount = 16;
+                _droneSpawner.MaxAliveCount = 14;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
+            case 10:
+                _droneSpawner.MaxSpawnCount = 18;
+                _droneSpawner.MaxAliveCount = 16;
+                SetObjective($"DESTROY DRONES\n 0/{_droneSpawner.MaxSpawnCount}");
+                break;
             default:
                 _droneSpawner.MaxSpawnCount = 20;
                 _droneSpawner.MaxAliveCount = 5;
